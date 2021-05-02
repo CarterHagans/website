92eceea982566a43d01db214fdfaa3b27b78a7c8
josh hagans <carterhagans@gmail.com>
2021-05-01 21:48:05 -0400

    5-1 commit

--- app.py
+++ app.py
@@ -1,12 +1,31 @@
 from flask import Flask, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask import request,flash,redirect
-from appcredientals import EMAIL,PASSWORD,RECEIVER,KEY
+from appcredientals import EMAIL,PASSWORD,RECEIVER,KEY,ADMIN_PASSWORD
 import smtplib,ssl
+from datetime import datetime
 
 
 app = Flask(__name__)
 app.secret_key = KEY
+db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+db.init_app(app)
+activeRequest = "hello"
+
+
+class PurchaseRequest(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    email = db.Column(db.String(80))
+    language = db.Column(db.String(100))
+    RequestedCode = db.Column(db.String)
+    date_created = db.Column(db.DateTime, default=datetime.now)
+
+    def __init__(self,email,language,RequestedCode):
+        self.email = email
+        self.language = language
+        self.RequestedCode = RequestedCode
+
 
 @app.route('/')
 def index():
@@ -55,7 +74,58 @@ def JavaScript():
 def Lua():
     return render_template("lua.html")
 
+@app.route("/purchase")
+def purchase():
+    return render_template("purchase.html")
 
+@app.route("/purchase/form",methods=["post","get"])
+def purchase_form():
+    if request.method == "POST":
+        user_email = request.form.get("requestemail")
+        language = request.form.get("requestlanguage")
+        thing_to_code = request.form.get("RequestedCode")
+        status = "Pending"
+        data = PurchaseRequest(user_email,language,thing_to_code)
+        db.session.add(data)
+        db.session.commit()
+        flash("Request sent!",'success')
+
+
+
+    return render_template(("purchase_form.html"))
+
+@app.route('/admin',methods=["post","get"])
+def admin():
+    if request.method == "POST":
+        entered_password = request.form.get("enteredadminpassword")
+        if entered_password == ADMIN_PASSWORD:
+            return redirect("/admin/panel")
+    return render_template("admin.html")
+
+@app.route(f'/admin/<activeRequest>',methods=["post","get"])
+def manage_request(activeRequest):
+    selectedRequest  = PurchaseRequest.query.filter_by(id=activeRequest).first()
+    if request.method == "GET":
+        return render_template("manage.html",identification=selectedRequest.id,email=selectedRequest.email,language=selectedRequest.language,codeRequest=selectedRequest.RequestedCode,date=selectedRequest.date_created)
+    if request.method == "POST":
+        if request.form["deletebtn"]:
+            db.session.delete(selectedRequest)
+            db.session.commit()
+            flash("Request has been deleted from the database.",'error')
+        else:
+            print("no")
+        return render_template("manage.html",identification=selectedRequest.id,email=selectedRequest.email,language=selectedRequest.language,codeRequest=selectedRequest.RequestedCode,date=selectedRequest.date_created)
+
+@app.route('/admin/panel',methods=["post","get"])
+def admin_panel():
+    if request.method == "POST":
+        ID = request.form.get("projectID")
+        global activeRequest
+        activeRequest = ID
+        return redirect(f"/admin/{activeRequest}",activeRequest)   
+    if request.method == "GET":
+        coderequests = PurchaseRequest.query.order_by(PurchaseRequest.date_created).all()
+        return render_template("panel.html",coderequests=coderequests)
 
 
 if __name__ == '__main__':
--- appcredientals.py
+++ appcredientals.py
@@ -2,3 +2,4 @@ EMAIL = "carterswebsitemanager@gmail.com"
 PASSWORD = "ChristmasPresent1"
 RECEIVER = 'carterhagansbusiness@gmail.com'
 KEY = "nsufdjvkskl nfiwo oi fjj0w9 proufweiosa fhoasr jw90oprujw90 riwe h  w9qru9we0 ruw9e "
+ADMIN_PASSWORD = "ChristmasPresent1"
